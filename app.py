6114d6336c8141b65adc60328156d8044dc77afd
Neelanjan Barman <77610806+BeyondNeel@users.noreply.github.com>
2024-01-07 18:33:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+
 # loading the saved models
-parkinson = pickle.load(open('parkinson.sav', 'rb'))
+
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
     
     selected = option_menu('Diagnosis Bar',
                           
-                          ['Parkinson'],
+                          ['Parkinsons Prediction'],
                           icons=['person'],
                           default_index=0)
+      
 
 # Parkinson's Prediction Page
+if (selected == "Parkinsons Prediction"):
+    
     # page title
-    st.title("Parkinson's Disease Detection System")
+    st.title("Parkinson's Disease Prediction")
     
     col1, col2, col3, col4, col5 = st.columns(5)  
     
